4a50de20af5e997cd2c779ec39b586a3a46d97b3
osamamalik234 <osamamalik9171@gmail.com>
2021-11-13 07:16:18 -0800

    Initial Commit

---
+++ src/com/company/Example/jav.java
@@ -0,0 +1,12 @@
+package com.company.Example;
+
+public class jav {
+    public static void main(String[] args) {
+        int a = 2;
+        int b = 3;
+        int sum = a + b;
+        System.out.println("The Sum of Two Numbers is: " + sum);
+    }
+
+
+}
